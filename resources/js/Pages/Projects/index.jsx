059748e18a4f656ca57b0c684f130964224f3411
Hyeladi Malgwi <46991842+H3SAN@users.noreply.github.com>
2024-07-11 13:26:46 +0300

    Additional changes and Implementation of Filtering

--- resources/js/Pages/Projects/index.jsx
+++ resources/js/Pages/Projects/index.jsx
@@ -1,9 +1,29 @@
+import SelectInput from "@/Components/SelectInput";
+import TextInput from "@/Components/TextInput";
 import Pagination from "@/Components/pagination";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants.jsx";
-import { Head, Link } from "@inertiajs/react";
+import { Head, Link, router } from "@inertiajs/react";
 
-export default function Index({ auth, projects }) {
+export default function Index({ auth, projects, queryParams = null, success }) {
+
+  queryParams = queryParams || {}
+  const searchFieldChanged = (name, value) => {
+    if (value) {
+      queryParams[name] = value
+    } else {
+      delete queryParams[name]
+    }
+
+    router.get(route('project.index'), queryParams)
+  }
+
+  const onKeyPress = (name, e) => {
+    if (e.key !== 'Enter') return;
+
+    searchFieldChanged(name, e.target.value);
+  }
+
   return (
     <AuthenticatedLayout
       user={auth.user}
@@ -26,15 +46,46 @@ export default function Index({ auth, projects }) {
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50
                  dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
                   <tr className="text-nowrap">
-                    <th className="px-3 py-2">ID</th>
+                    <th onClick={(e) => sortChanged('id')} className="px-3 py-2">ID</th>
                     <th className="px-3 py-2">Image</th>
-                    <th className="px-3 py-2">Name</th>
-                    <th className="px-3 py-2">status</th>
-                    <th className="px-3 py-2">Status</th>
-                    <th className="px-3 py-2">Created Date</th>
-                    <th className="px-3 py-2">Due Date</th>
-                    <th className="px-3 py-2">Created By</th>
+                    <th onClick={(e) => sortChanged('name')} className="px-3 py-2">Name</th>
+                    <th onClick={(e) => sortChanged('status')} className="px-3 py-2">status</th>
+                    <th onClick={(e) => sortChanged('created_at')} className="px-3 py-2">Created Date</th>
+                    <th onClick={(e) => sortChanged('due_date')} className="px-3 py-2">Due Date</th>
+                   (e)<th className="px-3 py-2">Created By</th>
                     <th className="px-3 py-2 rext-right">Actions</th>
+                  </tr>
+                </thead>
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50
+                 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                  <tr className="text-nowrap">
+                    <th className="px-3 py-2"></th>
+                    <th className="px-3 py-2"></th>
+                    <th className="px-3 py-2">
+                      <TextInput 
+                        className="w-full"
+                        defaultValue = {queryParams.name}
+                        placeholder="Project Name"
+                        onBlur={e => searchFieldChanged('name', e.target.value)}
+                        onKeyPress={(e) => onKeyPress('name', e)}
+                        />
+                    </th>
+                    <th className="px-3 py-2">
+                      <SelectInput className="w-full" 
+                      defaultValue = {queryParams.status}
+                        onChange={(e) => searchFieldChanged('status', e.target.value)
+                      }
+                    >
+                      <option value="">Select Status</option>
+                      <option value="pending">Pending</option>
+                      <option value="in_progress">In Progress</option>
+                      <option value="completed">Completed</option>
+                    </SelectInput>
+                    </th>
+                    <th className="px-3 py-2"></th>
+                    <th className="px-3 py-2"></th>
+                    <th className="px-3 py-2"></th>
+                    <th className="px-3 py-2 rext-right"></th>
                   </tr>
                 </thead>
                 <tbody>
