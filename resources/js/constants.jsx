d5e695ae141186076aca172e0a247f5d24e90713
Hyeladi Malgwi <46991842+H3SAN@users.noreply.github.com>
2024-11-21 09:26:00 +0200

    bug fixes

--- resources/js/constants.jsx
+++ resources/js/constants.jsx
@@ -8,6 +8,16 @@ export const PROJECT_STATUS_TEXT_MAP = {
   in_progress: "In Progress",
   completed: "Completed",
 };
+export const USER_STATUS_CLASS_MAP = {
+  pending: "bg-amber-500",
+  in_progress: "bg-blue-500",
+  completed: "bg-green-500",
+};
+export const USER_STATUS_TEXT_MAP = {
+  pending: "Pending",
+  in_progress: "In Progress",
+  completed: "Completed",
+};
 export const TASK_STATUS_CLASS_MAP = {
   pending: "bg-amber-500",
   in_progress: "bg-blue-500",
